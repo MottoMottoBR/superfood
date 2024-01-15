ff3ce2f97ef0604e6aef723199d0fc2ba9c43b06
MottoMottoBR <alessandro.aoc@gmail.com>
2024-01-14 22:17:24 -0400

    labelText adiconado

--- lib/widget/password_text_field.dart
+++ lib/widget/password_text_field.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 
 class PassWordTextField extends StatelessWidget {
-  const PassWordTextField({super.key});
+  final String? hintText;
+  final String? labelText;
+  const PassWordTextField({super.key, this.hintText, this.labelText});
 
   @override
   Widget build(BuildContext context) {
