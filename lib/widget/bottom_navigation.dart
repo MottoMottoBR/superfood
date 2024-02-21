aadbaf3adf503fcbbf4f41311afc91b3ebd6af3c
MottoMottoBR <alessandro.aoc@gmail.com>
2024-02-21 16:07:48 -0400

    SingleChildScrollView corrigido

---
+++ lib/widget/bottom_navigation.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+class BottomNavigationScrens extends StatelessWidget {
+  const BottomNavigationScrens({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(items: [],
+
+      ),
+    );
+  }
+}
