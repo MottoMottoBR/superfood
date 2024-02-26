1297be271a40282ccd798ead9bbdd0ebec6a4c15
MottoMottoBR <alessandro.aoc@gmail.com>
2024-02-26 17:13:27 -0400

    simboloSelecionado adicionado ao BottomNavigationBar

--- lib/pages/home_naviga.dart
+++ lib/pages/home_naviga.dart
@@ -6,6 +6,8 @@ import 'package:superfood/pages/pages_controller/perfil_page.dart';
 import 'package:superfood/pages/pages_controller/pesquisa_page.dart';
 import 'package:superfood/styles_patterns/app_colors.dart';
 
+import 'dart:ui' as ui;
+
 class HomeNaviga extends StatefulWidget {
   const HomeNaviga({super.key});
 
@@ -44,47 +46,56 @@ class _HomeNavigaState extends State<HomeNaviga> {
           FavoritosPage(),
         ],
       ),
-      bottomNavigationBar: BottomNavigationBar(
-        selectedItemColor: Colors.amber,
-        //unselectedItemColor: AppColors.backGroundColor,
-        currentIndex: paginaAtual,
-        items: const [
-          BottomNavigationBarItem(
-              icon: Icon(
-                Icons.home,
-                color: AppColors.black,
-              ),
-              label: simboloSelecionado),
-          BottomNavigationBarItem(
-              icon: Icon(
-                Icons.search,
-                color: AppColors.black,
-              ),
-              label: simboloSelecionado),
-          BottomNavigationBarItem(
-              icon: Icon(
-                Icons.shopping_bag_outlined,
-                color: AppColors.black,
-              ),
-              label: simboloSelecionado),
-          BottomNavigationBarItem(
-              icon: Icon(
-                Icons.favorite,
-                color: AppColors.black,
-              ),
-              label: simboloSelecionado),
-          BottomNavigationBarItem(
-              icon: Icon(
-                Icons.person,
-                color: AppColors.black,
-              ),
-              label: simboloSelecionado),
-        ],
-        onTap: (pagina) {
-          paginaControle.animateToPage(pagina,
-              duration: const Duration(milliseconds: 400), curve: Curves.ease);
-        },
-        backgroundColor: AppColors.whiteBackground,
+      bottomNavigationBar: Container(
+        clipBehavior: Clip.hardEdge,
+        decoration: BoxDecoration(
+            shape: BoxShape.rectangle,
+            borderRadius: BorderRadius.circular(8),
+            border: Border.all()),
+        child: BottomNavigationBar(
+          backgroundColor: AppColors.orange,
+          selectedItemColor: Colors.amber,
+          unselectedItemColor: Colors.transparent,
+          currentIndex: paginaAtual,
+          items: const [
+            BottomNavigationBarItem(
+                backgroundColor: AppColors.orange,
+                icon: Icon(
+                  Icons.home,
+                  color: AppColors.black,
+                ),
+                label: simboloSelecionado),
+            BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.search,
+                  color: AppColors.black,
+                ),
+                label: simboloSelecionado),
+            BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.shopping_bag_outlined,
+                  color: AppColors.black,
+                ),
+                label: simboloSelecionado),
+            BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.favorite,
+                  color: AppColors.black,
+                ),
+                label: simboloSelecionado),
+            BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.person,
+                  color: AppColors.black,
+                ),
+                label: simboloSelecionado),
+          ],
+          onTap: (pagina) {
+            paginaControle.animateToPage(pagina,
+                duration: const Duration(milliseconds: 400),
+                curve: Curves.ease);
+          },
+        ),
       ),
     );
   }
